9424bc8f6ba7fa825563e1a83b9550f27fae5ab5
lorenpabst@gmail.com <lorenpabst@gmail.com>
2018-05-31 16:37:37 -0600

    high score table shows up now

--- js/highscores.js
+++ js/highscores.js
@@ -1,4 +1,17 @@
 
+function getHighScores(){
+    // get high scores from the DB here
+    let fakeHighScores = [
+        {name: 'Jolly Pirate', score: 44323},
+        {name: 'Sully Parrot', score: 40123},
+        {name: 'One Eyed Mermaid', score: 32556},
+        {name: 'Tentative Gamer', score: 31856},
+        {name: 'Uncle Bob', score: 26891},
+    ];
+
+    return fakeHighScores;
+}
+
 function showHighScores(){
     document.getElementById('highScores').style.display = 'block';
 }
@@ -7,3 +20,19 @@ function closeHighScores(){
     document.getElementById('highScores').style.display = 'none';
 }
 
+let highScores = getHighScores();
+
+highScores.forEach( obj => {
+    let tableBody = document.getElementById('highScoresTableBody')
+    
+    let tr = document.createElement('tr');
+    tr.innerHTML = ''
+    +'<td>'
+        + obj.score
+    + '</td>'
+    + '<td>'
+        + obj.name
+    + '</td>'
+
+    tableBody.appendChild(tr);
+})
