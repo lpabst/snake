32f47ffa4f2891584b4521e952be97557e72db4c
lorenpabst@gmail.com <lorenpabst@gmail.com>
2018-05-31 17:04:48 -0600

    added gitignore and package.json

--- public/js/highscores.js
+++ public/js/highscores.js
@@ -1,23 +1,42 @@
 
-function getHighScores(){
-    // get high scores from the DB here
-    let fakeHighScores = [
-        {name: 'Jolly Pirate', score: 44323},
-        {name: 'Sully Parrot', score: 40123},
-        {name: 'One Eyed Mermaid', score: 32556},
-        {name: 'Tentative Gamer', score: 31856},
-        {name: 'Uncle Bob', score: 26891},
-    ];
-
-    return fakeHighScores;
-}
-
 function showHighScores(){
     document.getElementById('highScores').style.display = 'block';
 }
 
 function closeHighScores(){
     document.getElementById('highScores').style.display = 'none';
+}
+
+function getHighScores(){
+    // get high scores from the DB here
+    // let fakeHighScores = [
+    //     {name: 'Jolly Pirate', score: 44323},
+    //     {name: 'Sully Parrot', score: 40123},
+    //     {name: 'One Eyed Mermaid', score: 32556},
+    //     {name: 'Tentative Gamer', score: 31856},
+    //     {name: 'Uncle Bob', score: 26891},
+    // ];
+
+    var httpRequest;
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+        return false;
+    }
+
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('POST', '/api/runQuery');
+    httpRequest.send('queryString=' + encodeURIComponent('select * from snakehighscores order by score desc limit 5'));
+
+    function alertContents() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          if (httpRequest.status === 200) {
+            alert(httpRequest.responseText);
+          } else {
+            alert('There was a problem with the request.');
+          }
+        }
+    }
 }
 
 let highScores = getHighScores();
